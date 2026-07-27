de282491d9785a543c979d65f6acb52d791b593f
Ellie Williams <eawilliams47@tntech.edu>
2026-07-27 08:39:02 -0500

    added the app.py file

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from config import Config
+from database import create_database
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+create_database()
+
+@app.route('/')
+def home():
+    return "The school messenger is running."
+if __name__ == '__main__':
+    app.run(debug=True)
