e2b3b3cff8a064f5622e1b6766cd1a30e910757f
Ellie Williams <eawilliams47@tntech.edu>
2026-07-27 12:20:32 -0500

    Began adding database/SQl capabilities

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 #Starts flask
 from flask import Flask
 from config import Config
+from models import db, Recipient
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
--- models.py
+++ models.py
@@ -1,1 +1,32 @@
 #Defines database tables
+from flask_sqlalchemy import SQLAlchemy #creating an sqlalchemy object
+
+db = SQLAlchemy()
+
+class Recipient(db.Model):  #class is stored in the database
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    name = db.Column(
+        db.String(100),
+        nullable=False    #makes sure the field cannot be empty
+    )
+    phone = db.Column(
+        db.String(20),
+        nullable=False,
+        unique=True
+    )
+    recipient_type = db.Column(   #do not name it 'type' since that's an existing python function
+        db.String(20),
+        nullable=False
+    )
+    department = db.Column(
+        db.String(50)
+    )
+    active = db.Column(
+        db.Boolean,
+        default=True
+    )
+    def __repr__(self):   #allows us to print the actual name instead of the address
+        return f"<Recipient {self.name}>"
