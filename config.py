604cbede8cad428598b98d48f4403a301630baf7
Ellie Williams <eawilliams47@tntech.edu>
2026-07-30 09:00:57 -0500

    switched to sqlpostgres

--- config.py
+++ config.py
@@ -7,8 +7,19 @@ load_dotenv() #loads things inside the .env file
 class Config:
 
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
-                                        "sqlite:///mjca.db")
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
+
+    SQLALCHEMY_DATABASE_URI = (
+            database_url
+            or "sqlite:///mjca.db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
     TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
